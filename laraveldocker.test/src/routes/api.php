149eb6e53b1f28dc50acf734906771e88b61f592
hs-qwerty <hselviler1@gmail.com>
2021-11-27 23:15:42 +0300

    service commit
    service commit

--- laraveldocker.test/src/routes/api.php
+++ laraveldocker.test/src/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\ProductServiceController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::apiResource('products', ProductServiceController::class);
